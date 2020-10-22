59c3bd6e3850c14f32d1edaf1b68d34d7e92285f
yassinebichiou <yassine.bichiou@gmail.com>
2020-10-22 13:43:02 +0200

    first commit

---
+++ calculatrice.js
@@ -0,0 +1,23 @@
+var input1=Number;
+var input2=Number;
+tab=[]
+function A() {
+    var nb = document.getElementById("A").value;
+    // tab.push(A);
+    // tab=1;
+   console.log(nb);
+}
+
+function B (){
+   var nb = (document.getElementById("B").value)
+    console.log(nb);
+   
+}
+
+
+
+function plus() {
+var pl =  (document.getElementById("pl").value)
+var Somme = Number;
+pl=tab[1]+tab[2];
+console.log(Somme);
